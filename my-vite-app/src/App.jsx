e7bbf321a6b3096a4850f1dac89b7129139532ad
Mitch <mitchjoelklein@hotmail.com>
2024-11-22 10:38:34 +1100

    Update

--- my-vite-app/src/App.jsx
+++ my-vite-app/src/App.jsx
@@ -1,5 +1,4 @@
 // import { useState } from "react";
-import { BrowserRouter } from "react-router-dom";
 import { Outlet } from 'react-router-dom';
 import "./App.css";
 import Header from "./Header";
