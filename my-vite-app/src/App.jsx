d6f0d996743e87ba38469031838f80425db33845
Mitch <mitchjoelklein@hotmail.com>
2024-11-21 13:12:35 +1100

    AboutMe() component created

--- my-vite-app/src/App.jsx
+++ my-vite-app/src/App.jsx
@@ -2,12 +2,14 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Header from "./Header";
+import AboutMe from "./AboutMe";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Header />
+        <AboutMe />
       </BrowserRouter>
     </>
   );
