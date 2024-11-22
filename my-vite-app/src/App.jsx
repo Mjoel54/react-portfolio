2aac97955e6357343bcdf5687876ffe20be64857
Mitch <mitchjoelklein@hotmail.com>
2024-11-22 12:38:02 +1100

    Footer created

--- my-vite-app/src/App.jsx
+++ my-vite-app/src/App.jsx
@@ -2,12 +2,14 @@
 import { Outlet } from 'react-router-dom';
 import "./App.css";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
     <>
         <Header />
         <Outlet />
+        <Footer />
     </>
   );
 }
