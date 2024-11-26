0db7da8f40aefde168536ca055c41ee3fdc51efc
Mitch <mitchjoelklein@hotmail.com>
2024-11-26 21:18:09 +1100

    Spacing updates

--- my-vite-app/src/Header.jsx
+++ my-vite-app/src/Header.jsx
@@ -5,8 +5,8 @@ export default function Header() {
     console.log("Header is rendering");
   return (
     <>
-      <header className="d-flex flex-column flex-md-row justify-content-between align-items-center py-3 px-4 bg-slate fixed-top">
-      <h1 className="h1">Mitchell Klein</h1>
+      <header className="d-flex flex-column flex-md-row justify-content-between align-items-center py-4 px-4 bg-slate fixed-top text-white">
+      <h1 className="h1 px-4">Mitchell Klein</h1>
      
         <Navbar />
       </header>
